74909bd3f2717408a87abdf6f8f30c899377cba5
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:14:11 +0530

    Create StringConcatenation.java

---
+++ Strings/StringConcatenation.java
@@ -0,0 +1,11 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = "Viewers";
+
+        String s3 = s1 + s2;
+
+        System.out.println(s3);
+    }
+}
