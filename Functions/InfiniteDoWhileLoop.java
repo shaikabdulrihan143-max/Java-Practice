e4493b0e0f700d2b463359e645ca646a3b9d5c59
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:48:37 +0530

    Create InfiniteDoWhileLoop.java

---
+++ Functions/InfiniteDoWhileLoop.java
@@ -0,0 +1,13 @@
+public class InfiniteDoWhileLoop {
+
+    public static void infiniteDoWhileLoop(){
+        do{
+            System.out.println("INFINITE LOOP");
+        }while(true);
+    }
+
+    public static void main(String[] args){
+
+        infiniteDoWhileLoop();
+    }
+}
