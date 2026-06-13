7470a9be7dedd495e6721bf7dae063b47467c4a4
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:33:29 +0530

    Create InvertedHalfPyramid.java

---
+++ Patterns/InvertedHalfPyramid.java
@@ -0,0 +1,17 @@
+public class InvertedHalfPyramid {
+    public static void main(String[] args){
+
+        /* * * * *
+           * * *
+           * *
+           *
+        */
+
+        for(int i=4;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
