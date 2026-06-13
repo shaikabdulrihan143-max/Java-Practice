ee7f2f8f6fdec6f3f15fa267ff9bb0e3096e4c99
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:16:21 +0530

    Create IntegerToString.java

---
+++ Strings/IntegerToString.java
@@ -0,0 +1,10 @@
+public class IntegerToString {
+    public static void main(String[] args) {
+
+        int number=123;
+
+        String str=Integer.toString(number);
+
+        System.out.println(str.length());
+    }
+}
