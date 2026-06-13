340a57621be7d6c813434af46616d2587db23071
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:15:03 +0530

    Create StringComparison.java

---
+++ Strings/StringComparison.java
@@ -0,0 +1,11 @@
+public class StringComparison {
+    public static void main(String[] args) {
+
+        String s1="hii";
+        String s2="hiiiiiiii";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1==s2);
+        System.out.println(s1.compareTo(s2));
+    }
+}
