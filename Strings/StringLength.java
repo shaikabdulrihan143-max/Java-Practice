ba6c29d33ab7a461ca8dad4cb2f03486f6c34167
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:14:27 +0530

    Create StringLength.java

---
+++ Strings/StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static void main(String[] args) {
+
+        String str = "Abdul Rihan";
+
+        int len = str.length();
+
+        System.out.println(len);
+    }
+}
