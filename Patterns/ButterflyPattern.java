e32faf1de5bc8e950fb17764df81d47459f52c5b
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:37:22 +0530

    Create ButterflyPattern.java

---
+++ Patterns/ButterflyPattern.java
@@ -0,0 +1,48 @@
+public class ButterflyPattern {
+    public static void main(String[] args){
+
+        /* *             *
+           * *         * *
+           * * *     * * *
+           * * * * * * * *
+           * * * * * * * *
+           * * *     * * *
+           * *         * *
+           *             *
+        */
+
+        int n=4;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print("  ");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        for(int i=n;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print("  ");
+            }
+
+            for(int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
