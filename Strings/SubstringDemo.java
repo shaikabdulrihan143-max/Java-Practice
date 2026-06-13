3a7fd0275e001d4ce7e1fe2ce641f97570051ce9
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:15:22 +0530

    Create SubstringDemo.java

---
+++ Strings/SubstringDemo.java
@@ -0,0 +1,8 @@
+public class SubstringDemo {
+    public static void main(String[] args) {
+
+        String str="Hello Viewers My name is Abdul Rihan";
+
+        System.out.println(str.substring(6,str.length()));
+    }
+}
