4f65e559e419fa7677ea896213d7e0cccb04eb83
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:34:24 +0530

    Create NumberPyramid.java

---
+++ Patterns/NumberPyramid.java
@@ -0,0 +1,22 @@
+public class NumberPyramid {
+    public static void main(String[] args){
+
+        /* 1
+           12
+           123
+           1234
+           12345
+        */
+
+        int num=1;
+
+        for(int i=1;i<=5;i++){
+            num=1;
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
