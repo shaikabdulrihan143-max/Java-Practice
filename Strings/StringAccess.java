a79dc7a9531e5594fb6147814cd2f191f59f6df1
SHAIK.ABDUL RIHAN <shaikabdulrihan143@gmail.com>
2026-06-13 13:14:48 +0530

    Create StringAccess.java

---
+++ Strings/StringAccess.java
@@ -0,0 +1,10 @@
+public class StringAccess {
+    public static void main(String[] args) {
+
+        String str = "Hello Viewers";
+
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+",");
+        }
+    }
+}
